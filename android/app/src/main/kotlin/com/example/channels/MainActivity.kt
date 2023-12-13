31535599d0126033abcf87639cfb0f0b14d577bd
Berkay ÇAYLI <berkay.cayli@defacto.com>
2023-12-13 11:44:51 +0300

    update main activity

--- android/app/src/main/kotlin/com/example/channels/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/channels/MainActivity.kt
@@ -1,14 +1,19 @@
 package com.example.channels
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.EventChannel
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
 
-    // * dart tarafında oluşturduğumuz event channel ile aynı isme sahip olmalı
+    // * dart tarafındaki event channel adıyla aynı olması gerekiyor 
     private val networkEventChannel = "platform_channel_events/connectivity"
 
-    
-
-
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        EventChannel(flutterEngine.dartExecutor.binaryMessenger, networkEventChannel)
+            .setStreamHandler(NetworkStreamHandler(this))
+        
+       
+    }
 
 }
