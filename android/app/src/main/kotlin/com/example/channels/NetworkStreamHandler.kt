b64ec783e92b38a5470826473440e440a30ff198
Berkay ÇAYLI <berkay.cayli@defacto.com>
2023-12-13 11:47:58 +0300

    add network stream handler

---
+++ android/app/src/main/kotlin/com/example/channels/NetworkStreamHandler.kt
@@ -0,0 +1,39 @@
+package com.example.channels
+
+import android.app.Activity
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
+import android.os.Build
+import io.flutter.plugin.common.EventChannel
+
+class NetworkStreamHandler(private var activity: Activity?) : EventChannel.StreamHandler {
+
+    private var eventSink: EventChannel.EventSink? = null
+
+    override fun onListen(arguments: Any?, events: EventChannel.EventSink?) {}
+
+    override fun onCancel(arguments: Any?) {}
+
+    private val networkCallback = object : ConnectivityManager.NetworkCallback() {
+
+    }
+
+    private fun startListeningNetworkChanges() {
+        val manager = activity?.getSystemService(Context.CONNECTIVITY_SERVICE) as? ConnectivityManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            manager?.registerDefaultNetworkCallback(networkCallback)
+        } else {
+            val request = NetworkRequest.Builder()
+                .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+                .build()
+            manager?.registerNetworkCallback(request, networkCallback)
+        }
+    }
+
+    private fun stopListeningNetworkChanges() {
+        val manager = activity?.getSystemService(Context.CONNECTIVITY_SERVICE) as? ConnectivityManager
+        manager?.unregisterNetworkCallback(networkCallback)
+    }
+}
