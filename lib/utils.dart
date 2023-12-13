20fa0a58053584fde3185fdc5f33daecbcaab38e
Berkay ÇAYLI <berkay.cayli@defacto.com>
2023-12-13 11:22:49 +0300

    update utils

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,4 +1,5 @@
 import 'package:channels/constants.dart';
+import 'package:flutter/material.dart';
 
 /// Ağ durumları için [Connection] enum
 enum Connection {
@@ -32,4 +33,44 @@ Connection intToConnection(int connectionInt) {
       break;
   }
   return connection;
+}
+
+/// network olaylarını renk ile göstermek için kullanılır
+Color getConnectionColor(Connection connection) {
+  var color = Colors.red[900];
+  switch (connection) {
+    case Connection.wifi:
+      color = Colors.green[800];
+      break;
+    case Connection.cellular:
+      color = Colors.blue[900];
+      break;
+    case Connection.disconnected:
+      color = Colors.red[900];
+      break;
+    case Connection.unknown:
+      color = Colors.red[900];
+      break;
+  }
+  return color!;
+}
+
+/// network olaylarını kullanıcıya bir metin ile bildirmek için kullanılır
+String getConnectionMessage(Connection connection) {
+  var msg = 'Unknown connection';
+  switch (connection) {
+    case Connection.wifi:
+      msg = 'Connected to Wifi';
+      break;
+    case Connection.cellular:
+      msg = 'Connected to mobile data';
+      break;
+    case Connection.disconnected:
+      msg = 'Offline';
+      break;
+    case Connection.unknown:
+      msg = 'Unknown connection';
+      break;
+  }
+  return msg;
 }
