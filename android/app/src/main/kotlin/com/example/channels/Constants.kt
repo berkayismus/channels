fddd7d47408d585d0eb0d9f1fa2a66dd13ef6cb9
Berkay ÇAYLI <berkay.cayli@defacto.com>
2023-12-13 11:52:02 +0300

    add constants kt

---
+++ android/app/src/main/kotlin/com/example/channels/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.channels
+
+// * dart tarafında tanımladığımız sabitleri burada da tanımlıyoruz
+object Constants {
+    const val wifi = 0xFF
+    const val cellular = 0xEE
+    const val disconnected = 0xDD
+    const val unknown = 0xCC
+}
