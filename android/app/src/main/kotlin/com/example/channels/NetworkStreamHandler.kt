f41a504fd9fb5f199a6a23a079f28b54553ff067
Berkay ÇAYLI <berkay.cayli@defacto.com>
2023-12-13 13:10:48 +0300

    update manifest for permission

--- android/app/src/main/kotlin/com/example/channels/NetworkStreamHandler.kt
+++ android/app/src/main/kotlin/com/example/channels/NetworkStreamHandler.kt
@@ -7,6 +7,7 @@ import android.net.NetworkCapabilities
 import android.net.NetworkRequest
 import android.os.Build
 import io.flutter.plugin.common.EventChannel
+import android.net.Network
 
 class NetworkStreamHandler(private var activity: Activity?) : EventChannel.StreamHandler {
 
@@ -17,11 +18,31 @@ class NetworkStreamHandler(private var activity: Activity?) : EventChannel.Strea
         startListeningNetworkChanges()
     }
 
-    override fun onCancel(arguments: Any?) {}
-
-    private val networkCallback = object : ConnectivityManager.NetworkCallback() {
-
+    override fun onCancel(arguments: Any?) {
+        stopListeningNetworkChanges()
+        eventSink = null
+        activity = null
     }
+
+   private val networkCallback = object : ConnectivityManager.NetworkCallback() {
+    override fun onLost(network: Network) {
+        super.onLost(network)
+        // Notify Flutter that the network is disconnected
+        activity?.runOnUiThread { eventSink?.success(Constants.disconnected) }
+    }
+
+    override fun onCapabilitiesChanged(network: Network, netCap: NetworkCapabilities) {
+        super.onCapabilitiesChanged(network, netCap)
+        // Pick the supported network states and notify Flutter of this new state
+        val status =
+            when {
+                netCap.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> Constants.wifi
+                netCap.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> Constants.cellular
+                else -> Constants.unknown
+            }
+        activity?.runOnUiThread { eventSink?.success(status) }
+    }
+}
 
     private fun startListeningNetworkChanges() {
         val manager = activity?.getSystemService(Context.CONNECTIVITY_SERVICE) as? ConnectivityManager
