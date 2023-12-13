41c7f04f715a2c403f1334e06392aeb3e0587059
Berkay ÇAYLI <berkay.cayli@defacto.com>
2023-12-13 11:15:50 +0300

    add int to connection

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,3 +1,5 @@
+import 'package:channels/constants.dart';
+
 /// Ağ durumları için [Connection] enum
 enum Connection {
   /// bağlantı durumu [Constants.wifi] olduğunda
@@ -11,4 +13,23 @@ enum Connection {
 
   /// bağlantı durumu [Constants.unknown] olduğunda
   unknown
+}
+
+Connection intToConnection(int connectionInt) {
+  var connection = Connection.unknown;
+  switch (connectionInt) {
+    case Constants.wifi:
+      connection = Connection.wifi;
+      break;
+    case Constants.cellular:
+      connection = Connection.cellular;
+      break;
+    case Constants.disconnected:
+      connection = Connection.disconnected;
+      break;
+    case Constants.unknown:
+      connection = Connection.unknown;
+      break;
+  }
+  return connection;
 }
